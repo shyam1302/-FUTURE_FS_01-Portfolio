d80f2381471ea9150cb2ac7bd6584b96dd9e9231
shyam1302 <shyamk1302@gmail.com>
2026-01-02 14:03:13 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -33,7 +33,7 @@ function Footer() {
             </li>
             <li className="social-icons">
               <a
-                href=
+                href= "https://x.com/elonmusk"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
@@ -53,7 +53,7 @@ function Footer() {
             </li>
             <li className="social-icons">
               <a
-                href=
+                href="https://www.instagram.com/__elon__musk___official_/?hl=en"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
